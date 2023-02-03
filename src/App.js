7c0e2c5cc53561091862ffb9e33f9f84129bc9d8
Monil Andharia <andhariamonil@dal.ca>
2023-02-03 00:09:26 -0400

    Added Gitlab CICD

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>It works!</p>
+        <p>Test CICD works!</p>
         <a
           className='App-link'
           href='https://reactjs.org'
