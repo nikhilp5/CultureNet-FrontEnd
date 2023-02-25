a161be42478dfb0d293134be8fef1f0d1d3ab409
Monil Andharia <andhariamonil@dal.ca>
2023-02-25 18:31:15 -0400

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+// import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: lightBlue,
+//   },
+//   overrides: {
+//     MuiButton: {
+//       raisedPrimary: {
+//         color: 'white',
+//       },
+//     },
+//   },
+// });
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>It works!</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
