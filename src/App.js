522239a141cb267482dc544a22350f8f1bfcdad6
Rishi Vasa <rs310602@dal.ca>
2023-02-26 04:51:11 -0400

    Refactored Routing methodology to resolve error in production

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { appTheme } from './themes/theme';
-import AppRoutes from './routes/Routes';
+import Routes from './routes/Routes';
 import React, { Component } from 'react';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <ThemeProvider theme={appTheme}>
         <CssBaseline enableColorScheme />
-          <AppRoutes />
+          <Routes />
       </ThemeProvider>
     );
   }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes as Rt } from 'react-router-dom';
 import LandingPage from '../components/common/LandingPage';
 import Navbar from "../components/common/Navbar";
 import UserDashboard from "../components/user/UserDashboard"
 import AdminDashboard from "../components/admin/AdminDashboard"
 
-const AppRoutes = () => {
+const Routes = () => {
     return (
-        <Routes>
+        <Rt>
             <Route path = "/" element = {<LandingPage />} />
             <Route path = "/UserDashboard" element = {<UserDashboard />} />
             <Route path = "/AdminDashboard" element = {<AdminDashboard />} />
             <Route path = "/Navbar" element = {<Navbar />} />
-        </Routes>
+        </Rt>
     )
 };
 
-export default AppRoutes;
+export default Routes;
