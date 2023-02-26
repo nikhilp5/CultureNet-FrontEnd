5859977c3111a5ab07f8a66323936ca39ea17c31
Monil Andharia <andhariamonil@dal.ca>
2023-02-25 21:35:16 -0400

    Removed unwanted files

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@ import Navbar from './components/Navbar';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { appTheme } from './themes/theme';
 
-// import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: lightBlue,
-//   },
-//   overrides: {
-//     MuiButton: {
-//       raisedPrimary: {
-//         color: 'white',
-//       },
-//     },
-//   },
-// });
-
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
