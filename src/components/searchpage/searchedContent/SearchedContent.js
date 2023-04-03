889cb7debcd46af20b31951a8281675ee94d92af
Nikhil Sugathan Panikkassery <nk561034@dal.ca>
2023-04-03 17:37:39 -0300

    frontend commit for search and watchlist

--- src/components/searchpage/searchedContent/SearchedContent.js
+++ src/components/searchpage/searchedContent/SearchedContent.js
@@ -1,16 +1,18 @@
 import {
+  Backdrop,
+  CircularProgress,
   createTheme,
   Divider,
   responsiveFontSizes,
   ThemeProvider,
   Typography,
-} from '@mui/material';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Navbar from '../../common/Navbar';
-import SearchedBooks from '../searchedBooks/SearchedBooks';
-import SearchedMovies from '../searchedMovies/SearchedMovies';
-import SearchedUsers from '../searchedUsers/SearchedUsers';
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import SearchedBooks from "../searchedBooks/SearchedBooks";
+import SearchedMovies from "../searchedMovies/SearchedMovies";
+import SearchedUsers from "../searchedUsers/SearchedUsers";
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
@@ -19,90 +21,44 @@ const SearchedContent = () => {
   const [movieResults, setMovieResults] = useState([]);
   const [bookResults, setBookResults] = useState([]);
   const [userResults, setUserResults] = useState([]);
+  const [buttonClick, setButtonClick] = useState(false);
 
   const { state } = useLocation();
   const searchTerm = state;
 
-  const obj = {
-    movies: [
-      {
-        id: 1,
-        title: 'Movie1',
-      },
-      {
-        id: 2,
-        title: 'Movie2',
-      },
-    ],
-    books: [
-      {
-        id: 1,
-        title: 'Book1',
-      },
-      {
-        id: 2,
-        title: 'Book2',
-      },
-    ],
-    users: [
-      {
-        id: 1,
-        title: 'User1',
-      },
-      {
-        id: 2,
-        title: 'User2',
-      },
-    ],
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(!open);
   };
 
   useEffect(() => {
-    var movieArray = [];
-    var bookArray = [];
-    var userArray = [];
-    for (var key in obj) {
-      var value = obj[key];
-      for (var j = 0; j < value.length; j++) {
-        if (key === 'movies') {
-          if (
-            value[j].title != null &&
-            searchTerm != null &&
-            value[j].title.toLowerCase().includes(searchTerm.toLowerCase())
-          ) {
-            movieArray.push(value[j]);
-          }
-        }
-        if (key === 'books') {
-          if (
-            value[j].title != null &&
-            searchTerm != null &&
-            value[j].title.toLowerCase().includes(searchTerm.toLowerCase())
-          ) {
-            bookArray.push(value[j]);
-          }
-        }
-        if (key === 'users') {
-          if (
-            value[j].title != null &&
-            searchTerm != null &&
-            value[j].title.toLowerCase().includes(searchTerm.toLowerCase())
-          ) {
-            userArray.push(value[j]);
-          }
-        }
-      }
-    }
-    setMovieResults(movieArray);
-    setBookResults(bookArray);
-    setUserResults(userArray);
-  }, [searchTerm]);
+    handleOpen();
+    axios
+      .get(
+        "https://culturenet-apis-develop.netlify.app/.netlify/functions/api/search/" +
+          searchTerm
+      )
+      .then((res) => {
+        setMovieResults(res.data.result.movies);
+        setBookResults(res.data.result.books);
+        setUserResults(res.data.result.users);
+        handleClose();
+      })
+      .catch((error) => {
+        alert("Error- " + error);
+        handleClose();
+      });
+  }, [searchTerm, buttonClick]);
 
   return (
     <div>
       <ThemeProvider theme={theme}>
         <Typography
-          variant='h4'
-          style={{ fontWeight: '600' }}
+          variant="h4"
+          style={{ fontWeight: "600" }}
           mt={2}
           ml={10}
           mb={2}
@@ -110,9 +66,23 @@ const SearchedContent = () => {
           Search results for "{searchTerm}"
         </Typography>
         <Divider />
-        <SearchedMovies movieResults={movieResults}></SearchedMovies>
-        <SearchedBooks bookResults={bookResults}></SearchedBooks>
+        <SearchedMovies
+          movieResults={movieResults}
+          buttonClick={buttonClick}
+          setButtonClick={setButtonClick}
+        ></SearchedMovies>
+        <SearchedBooks
+          bookResults={bookResults}
+          buttonClick={buttonClick}
+          setButtonClick={setButtonClick}
+        ></SearchedBooks>
         <SearchedUsers userResults={userResults}></SearchedUsers>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={open}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
       </ThemeProvider>
     </div>
   );
--- src/components/watchlist/watchListContent/WatchListContent.js
+++ src/components/watchlist/watchListContent/WatchListContent.js
@@ -1,27 +1,80 @@
-import { Container, Typography } from '@mui/material';
-import { useSelector } from 'react-redux';
-import Navbar from '../../common/Navbar';
-import { selectAllWatchlistContent } from '../addContent/contentsSlice';
-import ContentGrid from '../contentShowcaseUI/contentGrid';
-import PageToggle from '../pageToggle/PageToggle';
+import {
+  Backdrop,
+  CircularProgress,
+  createTheme,
+  Divider,
+  responsiveFontSizes,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SearchedBooks from "../../searchpage/searchedBooks/SearchedBooks";
+import SearchedMovies from "../../searchpage/searchedMovies/SearchedMovies";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 const WatchListContent = () => {
-  const watchListContents = useSelector(selectAllWatchlistContent);
+  const [movieResults, setMovieResults] = useState([]);
+  const [bookResults, setBookResults] = useState([]);
+  const [buttonClick, setButtonClick] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    handleOpen();
+    axios
+      .get(
+        "https://culturenet-apis-develop.netlify.app/.netlify/functions/api/getWatchlist/"
+      )
+      .then((res) => {
+        setMovieResults(res.data.result.movies);
+        setBookResults(res.data.result.books);
+        handleClose();
+      })
+      .catch((error) => {
+        alert("Error- " + error);
+        handleClose();
+      });
+  }, [buttonClick]);
+
   return (
     <div>
-      <PageToggle />
-      <Container fixed>
-        <Typography variant='h4' style={{ fontWeight: '600' }} mt={4}>
+      <ThemeProvider theme={theme}>
+        <Typography
+          variant="h4"
+          style={{ fontWeight: "600" }}
+          mt={2}
+          ml={10}
+          mb={2}
+        >
           My WatchList
         </Typography>
-        {watchListContents.length > 0 ? (
-          <>
-            <ContentGrid contents={watchListContents} type='watchlist' />
-          </>
-        ) : (
-          <h2>No Content Added</h2>
-        )}
-      </Container>
+        <Divider />
+        <SearchedMovies
+          movieResults={movieResults}
+          buttonClick={buttonClick}
+          setButtonClick={setButtonClick}
+        ></SearchedMovies>
+        <SearchedBooks
+          bookResults={bookResults}
+          buttonClick={buttonClick}
+          setButtonClick={setButtonClick}
+        ></SearchedBooks>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={open}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </ThemeProvider>
     </div>
   );
 };
