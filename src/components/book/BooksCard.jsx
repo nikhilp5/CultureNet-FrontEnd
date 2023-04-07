e1658a6ee060b2659f440398871243e642bd0178
Ronil Patel <ronil.patel@dal.ca>
2023-04-07 14:54:06 -0300

    Updated Books page for Performant API call, improved design

--- src/components/book/BooksCard.jsx
+++ src/components/book/BooksCard.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 import { Card, CardActions, CardContent, CardMedia, Rating, Typography } from '@mui/material';
 
 
@@ -8,8 +10,8 @@ const BooksCard = ({ book }) => {
 
             <CardMedia
                 component="img"
-                sx={{ pt: 1}}
-                image={book.img}
+                sx={{ width: '100%', height: 400 }}
+                image={book.image_url}
             />
 
             <CardContent>
@@ -19,18 +21,19 @@ const BooksCard = ({ book }) => {
                 </Typography>
                 
                 <Typography variant="subtitle1" component="h2">
-                    By {book.author}
+                    By {book.authors}
                 </Typography>
                 
                 <Typography variant="subtitle1" component="h2">
-                    {book.category}
+                    {book.genre}
                 </Typography>
             
             </CardContent>
 
-            <CardActions style={{ flex: 1, justifyContent: 'space-evenly' }}>
-                <Rating name="read-only" value={book.rating} max={5} precision={0.5} readOnly />
+            <CardActions style={{ display: 'flex', justifyContent: 'flex-start' }}>
+                <Rating name="read-only" value={3} max={5} precision={0.5} readOnly />
             </CardActions>
+
 
         </Card>
     </div>
