f4ee1cc0ab2c41fb16f8e03694d50130ea2891a3
Monil Andharia <andhariamonil@dal.ca>
2023-04-09 18:17:45 -0300

    Added pagination for books and music

--- src/components/book/BooksGrid.jsx
+++ src/components/book/BooksGrid.jsx
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Container, CssBaseline, Grid, Pagination, ThemeProvider } from '@mui/material';
+import React, { useState, useContext, useEffect } from "react";
+import {
+  Container,
+  CssBaseline, Grid,
+  Pagination,
+  ThemeProvider,
+  useMediaQuery
+} from '@mui/material';
 import { appTheme } from '../../themes/theme';
 
 import { useNavigate } from "react-router";
 import BooksCard from './BooksCard';
+import { UserContext } from "../../utils/UserContext";
 
 const bookData = require("../../data/db.json");
 const items = bookData.books;
@@ -11,70 +18,98 @@ const items = bookData.books;
 
 export default function BookCards() {
 
-    const [books, setBooks] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage, setBooksPerPage] = useState(9);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { setOpenSnackbar, setSnackbarMessage, setSnackbarSeverity } = useContext(UserContext);
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  const startIndex = (currentPage - 1) * booksPerPage;
+  const endIndex = startIndex + booksPerPage;
+  const totalPages = Math.ceil(books.length / booksPerPage);
 
-    useEffect(() => {
-        setLoading(true);
-        const fetchBooks = async () => {
-          try {
-            if(!localStorage.getItem("token")) {navigate("/")};
 
-            const response = await fetch(
-              `${process.env.REACT_APP_BASE_URL}`+"/books"
-            ,  {
-              headers: {
-                "Authorization": `Bearer ${localStorage.getItem("token")}`
-              }
-            });
-            const data = await response.json();
-            console.log(data)
-            setBooks(data);
-            setLoading(false);
-          } catch (error) {
-            console.log(error);
-            setLoading(false);
-          }
-        };
-        fetchBooks();
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    const fetchBooks = async () => {
+      try {
+        if (!localStorage.getItem("token")) { navigate("/"); };
 
-    const handleBookClick = (id) => {
-      navigate("/bookdetail", { state: { id } });
-      console.log(id)
+        const response = await fetch(
+          `${process.env.REACT_APP_BASE_URL}` + "/books"
+          , {
+            headers: {
+              "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+          });
+        const data = await response.json();
+        setBooks(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
     };
+    fetchBooks();
+  }, []);
 
-    return (
+  useEffect(() => {
+    if (isSmallScreen) {
+      setBooksPerPage(1);
+    } else {
+      setBooksPerPage(9);
+    }
+  }, [isSmallScreen]);
 
-        <ThemeProvider theme={appTheme}>
-            <CssBaseline />
-            <Container maxWidth="md" my={5} style={{ marginTop: '20px' }}>
-            
-                <Grid container spacing={4}>
-                    
-                    {books.map((book) => (
 
-                        <Grid item key={book} md={4}>
-                            
-                            <BooksCard 
-                              book={book}
-                              onBookClick={handleBookClick}
-                            />
-                        
-                        </Grid>
-                    ))}
+  const handleBookClick = (id) => {
+    navigate("/bookdetail", { state: { id } });
+    console.log(id);
+  };
 
-                </Grid>
+  return (
 
-                <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{ my: '1rem' }}>
-                    <Grid item xs={12}>
-                        <Pagination count={Math.ceil(10 / 3)} style={{ flex: 1, justifyContent: 'center' }} sx={{ my: 3 }} showFirstButton showLastButton />
-                    </Grid>
-                </Grid>
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline />
+      <Container maxWidth="md" my={5} style={{ marginTop: '20px' }}>
 
-            </Container>
-        </ThemeProvider >
-    );
+        <Grid container spacing={4}>
+
+          {books.slice(startIndex, endIndex).map((book) => (
+
+            <Grid item key={book} md={4}>
+
+              <BooksCard
+                book={book}
+                onBookClick={handleBookClick}
+              />
+
+            </Grid>
+          ))}
+
+        </Grid>
+
+        {!isSmallScreen && (
+          <Pagination
+            count={totalPages}
+            page={currentPage}
+            onChange={(event, value) => setCurrentPage(value)}
+            sx={{ display: 'flex', justifyContent: 'center', marginTop: 4 }}
+          />
+        )}
+        {isSmallScreen && (
+          <Grid container justifyContent="center" sx={{ marginTop: 4 }}>
+            <Pagination
+              count={totalPages}
+              page={currentPage}
+              onChange={(event, value) => setCurrentPage(value)}
+            />
+          </Grid>
+        )}
+
+      </Container>
+    </ThemeProvider >
+  );
 }
